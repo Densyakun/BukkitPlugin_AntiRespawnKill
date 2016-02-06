6663e87f22e4b1b45924f091870e8e612d19bfa9
電車君 <Densyakun1128@gmail.com>
2016-02-07 07:14:17 +0900

    無駄なものが入っていたので削除

--- src/org/densyakun/csvm/ColumnNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.densyakun.csvm;
-@SuppressWarnings("serial")
-public class ColumnNotFoundException extends Throwable {
-	ColumnNotFoundException(int l, int c) {
-		super("Line: " + l + " Column: " + c);
-	}
-}
--- src/org/densyakun/csvm/LineNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.densyakun.csvm;
-@SuppressWarnings("serial")
-public class LineNotFoundException extends Throwable {
-	LineNotFoundException(int l) {
-		super("Line: " + l);
-	}
-}
